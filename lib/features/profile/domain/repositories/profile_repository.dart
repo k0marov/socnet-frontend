cb961de181167bd18688d79deebe27ed42b8ed9b
Sam Komarov <sam@skomarov.com>
2022-06-25 14:08:46 +0300

    implement UpdateAvatar in domain layer

--- lib/features/profile/domain/repositories/profile_repository.dart
+++ lib/features/profile/domain/repositories/profile_repository.dart
@@ -3,9 +3,12 @@ import 'package:socnet/core/error/failures.dart';
 import 'package:socnet/features/profile/domain/entities/profile.dart';
 import 'package:socnet/features/profile/domain/values/profile_update.dart';
 
+import '../values/avatar.dart';
+
 abstract class ProfileRepository {
   Future<Either<Failure, Profile>> getMyProfile();
   Future<Either<Failure, List<Profile>>> getFollows(Profile profile);
   Future<Either<Failure, void>> toggleFollow(Profile profile);
   Future<Either<Failure, Profile>> updateProfile(ProfileUpdate update);
+  Future<Either<Failure, AvatarURL>> updateAvatar(AvatarFile newAvatar);
 }
---
+++ lib/features/profile/domain/values/avatar.dart
@@ -0,0 +1,9 @@
+import 'package:equatable/equatable.dart';
+import 'package:socnet/core/simple_file/simple_file.dart';
+
+typedef AvatarFile = SimpleFile;
+typedef AvatarURL = String;
+
+// TODO: make backend return null for "no avatar" instead of an empty string
+
+
