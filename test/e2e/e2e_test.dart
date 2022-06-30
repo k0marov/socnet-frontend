cb860d4b4409d0b1b2810116842c701a07e0162e
Sam Komarov <sam@skomarov.com>
2022-06-30 15:33:18 +0300

    add following a profile to the e2e test

--- test/e2e/e2e_test.dart
+++ test/e2e/e2e_test.dart
@@ -7,6 +7,8 @@ import 'package:socnet/core/usecases/usecase.dart';
 import 'package:socnet/di.dart' as di;
 import 'package:socnet/features/auth/domain/usecases/login_usecase.dart';
 import 'package:socnet/features/auth/domain/usecases/register_usecase.dart';
+import 'package:socnet/features/profile/domain/entities/profile.dart';
+import 'package:socnet/features/profile/domain/usecases/get_profile.dart';
 import 'package:socnet/features/profile/domain/usecases/profile_params.dart';
 
 import '../core/helpers/helpers.dart';
@@ -47,6 +49,8 @@ void main() {
     expect(profile1.username, "sam");
     expect(profile1.isMine, true);
     expect(profile1.isFollowed, false);
+    expect(profile1.follows, 0);
+    expect(profile1.followers, 0);
 
     // logout
     var isSuccessful = await usecases.logout(NoParams());
@@ -57,9 +61,37 @@ void main() {
     final profile2 = forceRight(await usecases.getMyProfile(NoParams()));
     expect(profile2.username, "test");
 
+    // get first profile, viewing it from second user
+    final gotProfile1 = forceRight(await usecases.getProfile(ProfileIDParams(profile1.id)));
+    final wantProfile1 = Profile(
+      id: profile1.id,
+      username: profile1.username,
+      about: profile1.about,
+      avatarUrl: profile1.avatarUrl,
+      followers: profile1.followers,
+      follows: profile1.follows,
+      isMine: false, // important part
+      isFollowed: profile1.isFollowed,
+    );
+    expect(gotProfile1, wantProfile1);
+
     // follow first user from second user
-    isSuccessful = await usecases.toggleFollow(ProfileParams(profile: profile1));
+    isSuccessful = await usecases.toggleFollow(ProfileParams(profile: gotProfile1));
     expect(isSuccessful.isRight(), true);
+
+    // assert it was followed
+    final wantProfile1Followed = Profile(
+      id: profile1.id,
+      username: profile1.username,
+      about: profile1.about,
+      avatarUrl: profile1.avatarUrl,
+      follows: profile1.follows,
+      followers: 1,
+      isMine: false,
+      isFollowed: true,
+    );
+    final gotProfile1Followed = forceRight(await usecases.getProfile(ProfileIDParams(profile1.id)));
+    expect(gotProfile1Followed, wantProfile1Followed);
   });
 }
 
