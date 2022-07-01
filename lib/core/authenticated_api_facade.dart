38c5b59496ee6cabf98d55b2d83fd824aff02bf3
Sam Komarov <sam@skomarov.com>
2022-07-01 07:10:03 +0300

    add logging to authenticated_api_facade.dart

--- lib/core/authenticated_api_facade.dart
+++ lib/core/authenticated_api_facade.dart
@@ -2,9 +2,10 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:http/http.dart' as http;
+import 'package:socnet/core/debug.dart';
 import 'package:socnet/core/error/exceptions.dart';
-import 'package:socnet/core/simple_file/simple_file.dart';
-import 'package:socnet/core/usecases/usecase.dart';
+import 'package:socnet/core/simple_file.dart';
+import 'package:socnet/core/usecase.dart';
 import 'package:socnet/features/auth/domain/entities/token_entity.dart';
 import 'package:socnet/features/auth/domain/usecases/get_auth_token_usecase.dart';
 
@@ -20,36 +21,54 @@ class AuthenticatedAPIFacade {
   Future<http.Response> get(String endpoint, Map<String, String> body) async {
     final tokenEntity = await _obtainTokenOrThrow();
     final headers = _getHeaders(tokenEntity.token);
-    return _httpClient.get(_composeURL(endpoint, body), headers: headers);
+    final url = _composeURL(endpoint, body);
+    printDebug("GET $url");
+    final response = await _httpClient.get(url, headers: headers);
+    printResponse(response);
+    return response;
   }
 
   Future<http.Response> post(String endpoint, Map<String, dynamic> body) async {
     final tokenEntity = await _obtainTokenOrThrow();
     final headers = _getHeaders(tokenEntity.token);
-    return _httpClient.post(
-      _composeURL(endpoint),
-      body: json.encode(body),
+    final url = _composeURL(endpoint);
+    final bodyJson = json.encode(body);
+    printDebug("POST $url: $bodyJson");
+    final response = await _httpClient.post(
+      url,
+      body: bodyJson,
       headers: headers,
     );
+    printResponse(response);
+    return response;
   }
 
   Future<http.Response> delete(String endpoint) async {
     final tokenEntity = await _obtainTokenOrThrow();
     final headers = _getHeaders(tokenEntity.token);
-    return _httpClient.delete(
-      _composeURL(endpoint),
+    final url = _composeURL(endpoint);
+    printDebug("DELETE $url");
+    final response = await _httpClient.delete(
+      url,
       headers: headers,
     );
+    printResponse(response);
+    return response;
   }
 
   Future<http.Response> put(String endpoint, Map<String, dynamic> body) async {
     final tokenEntity = await _obtainTokenOrThrow();
     final headers = _getHeaders(tokenEntity.token);
-    return _httpClient.put(
-      _composeURL(endpoint),
-      body: json.encode(body),
+    final url = _composeURL(endpoint);
+    final bodyJson = json.encode(body);
+    printDebug("PUT $url: $bodyJson");
+    final response = await _httpClient.put(
+      url,
+      body: bodyJson,
       headers: headers,
     );
+    printResponse(response);
+    return response;
   }
 
   Future<http.Response> sendFiles(
@@ -60,10 +79,11 @@ class AuthenticatedAPIFacade {
   ) async {
     final token = await _obtainTokenOrThrow();
     final headers = _getHeaders(token.token);
-    final request = http.MultipartRequest(method, _composeURL(endpoint));
+    final url = _composeURL(endpoint);
+    final request = http.MultipartRequest(method, url);
 
     for (final fileEntry in files.entries) {
-      final fileBytes = await File(fileEntry.value.path).readAsBytes();
+      final fileBytes = File(fileEntry.value.path).readAsBytesSync();
       request.files.add(http.MultipartFile.fromBytes(fileEntry.key, fileBytes, filename: 'file'));
     }
 
@@ -76,8 +96,12 @@ class AuthenticatedAPIFacade {
       request.headers[header.key] = header.value;
     }
 
-    final response = await _httpClient.send(request);
-    return http.Response.fromStream(response);
+    printDebug("$method $url\nFiles: $files\nData: $data");
+
+    final responseStream = await _httpClient.send(request);
+    final response = await http.Response.fromStream(responseStream);
+    printResponse(response);
+    return response;
   }
 
   Future<Token> _obtainTokenOrThrow() async {
