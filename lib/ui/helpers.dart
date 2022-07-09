8a9f82939bc3cb6fe4ede1886741326394cc8399
Sam Komarov <sam@skomarov.com>
2022-07-09 09:16:32 +0300

    make getting square image a helper

--- lib/ui/helpers.dart
+++ lib/ui/helpers.dart
@@ -1,6 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:image_cropper/image_cropper.dart';
+import 'package:image_picker/image_picker.dart';
+
+import '../logic/core/simple_file.dart';
 
 Route _routeFromWidget(Widget widget) => MaterialPageRoute(builder: (_) => widget);
 
 void pushPage(BuildContext ctx, Widget page) => Navigator.of(ctx).push(_routeFromWidget(page));
 void replacePage(BuildContext ctx, Widget page) => Navigator.of(ctx).pushReplacement(_routeFromWidget(page));
+
+Future<SimpleFile?> chooseSquareImage() async {
+  final imageFile = await ImagePicker().pickImage(source: ImageSource.gallery);
+  if (imageFile == null) return null;
+  final croppedImage = await ImageCropper()
+      .cropImage(sourcePath: imageFile.path, aspectRatio: const CropAspectRatio(ratioX: 1, ratioY: 1));
+  if (croppedImage == null) return null;
+  return SimpleFile(croppedImage.path);
+}
--- lib/ui/pages/my_profile_page.dart
+++ lib/ui/pages/my_profile_page.dart
@@ -1,16 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:image_cropper/image_cropper.dart';
-import 'package:image_picker/image_picker.dart';
-import 'package:socnet/logic/core/simple_file.dart';
 import 'package:socnet/logic/di.dart';
-import 'package:socnet/logic/features/profile/domain/values/profile_update.dart';
 import 'package:socnet/logic/features/profile/presentation/my_profile/bloc/my_profile_bloc.dart';
-import 'package:socnet/ui/pages/post_creation_page.dart';
-import 'package:socnet/ui/pages/posts_page.dart';
-import 'package:uuid/uuid.dart';
-
-import '../../logic/features/auth/presentation/auth_gate_cubit/auth_gate_cubit.dart';
+import 'package:socnet/ui/widgets/failure_listener.dart';
 
 class MyProfilePage extends StatelessWidget {
   const MyProfilePage({Key? key}) : super(key: key);
@@ -30,77 +22,51 @@ class _InternalPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            BlocBuilder<MyProfileBloc, MyProfileState>(
-              bloc: context.read(),
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 20, horizontal: 30),
+          child: FailureListener<MyProfileBloc, MyProfileState>(
+            getFailure: (state) => state is MyProfileFailure ? state.failure : null,
+            child: BlocBuilder<MyProfileBloc, MyProfileState>(
               builder: (context, state) {
-                if (state is MyProfileInitial) {
-                  return Container();
-                } else if (state is MyProfileLoading) {
-                  return const CircularProgressIndicator();
+                if (state is MyProfileInitial || state is MyProfileLoading) {
+                  return CircularProgressIndicator();
                 } else if (state is MyProfileLoaded) {
-                  return Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-                    if (state.nonFatalFailure != null) Text(state.nonFatalFailure.toString()),
-                    if (state.profile.avatarUrl != null)
-                      Image(
-                        key: Key(const Uuid().v1()),
-                        image: NetworkImage("https://" + state.profile.avatarUrl!),
+                  return Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
+                    SizedBox(
+                      width: 200,
+                      height: 200,
+                      child: CircleAvatar(
+                        foregroundImage: NetworkImage("https://" + state.profile.avatarUrl!),
                       ),
-                    Text(state.profile.username),
-                    Text(state.profile.about),
-                    Text(state.profile.followers.toString()),
-                    Text(state.profile.follows.toString()),
-                    TextButton(
-                      child: const Text("Update about to random string"),
-                      onPressed: () => context.read<MyProfileBloc>().add(
-                            ProfileUpdateRequested(
-                              profileUpdate: ProfileUpdate(newAbout: const Uuid().v1()),
-                            ),
-                          ),
                     ),
-                    TextButton(
-                      child: const Text("Update Avatar"),
-                      onPressed: () async {
-                        final imageFile = await ImagePicker().pickImage(source: ImageSource.gallery);
-                        if (imageFile != null) {
-                          final croppedImage = await ImageCropper().cropImage(sourcePath: imageFile.path);
-                          if (croppedImage == null) return;
-                          context
-                              .read<MyProfileBloc>()
-                              .add(AvatarUpdateRequested(newAvatar: SimpleFile(croppedImage.path)));
-                        }
-                      },
-                    ),
-                    TextButton(
-                      onPressed: () => Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context) {
-                        return PostsPage(profileId: state.profile.id);
-                      })),
-                      child: Text("View Posts"),
+                    TextFormField(
+                      initialValue: state.profile.about,
                     )
+                    // SizedBox(height: 15),
+                    // Text(
+                    //   "@${state.profile.username}",
+                    //   style: Theme.of(context).textTheme.headlineSmall,
+                    // ),
+                    // SizedBox(height: 15),
+                    // Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+                    //   Column(
+                    //     children: [Text("Followers"), SizedBox(height: 5), Text(state.profile.followers.toString())],
+                    //   ),
+                    //   Column(
+                    //     children: [Text("Follows"), SizedBox(height: 5), Text(state.profile.follows.toString())],
+                    //   ),
+                    // ]),
+                    // SizedBox(height: 15),
+                    // Text(state.profile.about),
                   ]);
-                } else if (state is MyProfileFailure) {
-                  return Text(state.failure.toString());
                 } else {
-                  return Container();
+                  return Text("Error");
                 }
               },
             ),
-            TextButton(
-              child: const Text("Logout"),
-              onPressed: () => context.read<AuthGateCubit>().logout(),
-            ),
-            TextButton(
-              onPressed: () => Navigator.of(context).push(
-                MaterialPageRoute(builder: (BuildContext context) {
-                  return const PostCreationPage();
-                }),
-              ),
-              child: const Text("New Post"),
-            ),
-          ],
+          ),
         ),
       ),
     );
